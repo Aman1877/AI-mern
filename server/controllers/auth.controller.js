5d4cea07a06519e61043ea678afb51509b7c1ffc
Aman1877 <122619515+Aman1877@users.noreply.github.com>
2026-06-30 03:05:40 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -34,7 +34,12 @@ export const googleAuth = async (req, res) => {
 
 export const logOut = async (req, res) => {
   try {
-    await res.clearCookie("token");
+    await res.clearCookie("token", {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  path: "/",
+});
     return res.status(200).json({ message: "Logout Successfully" });
   } catch (error) {
     return res.status(500).json({ message: `Logout error ${error}` });
